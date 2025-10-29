8052140817cd5e9371fa28387d17542f2fefcb1e
Jose47Morales <josemoralesleon58@gmail.com>
2025-10-29 17:21:27 -0500

    Force Display PHP errors

--- public/index.php
+++ public/index.php
@@ -18,6 +18,10 @@ require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
