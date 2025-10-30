40be9822ac0794c3b65e5c33b18697081eaf4dbc
Jose47Morales <josemoralesleon58@gmail.com>
2025-10-29 21:13:44 -0500

    Fix Vite base path for Heroku

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    base: '',
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+        rollupOptions: {
+            input: 'resources/js/app.js',
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
